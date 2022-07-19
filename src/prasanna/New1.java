ba3314f61426a4da5fe17bb7881b9086959f78a4
munna <prasannacharaya@gmail.com>
2022-07-19 20:43:06 +0530

    new update

---
+++ src/prasanna/New1.java
@@ -0,0 +1,12 @@
+package prasanna;
+
+public class New1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("this workss  guys");
+			
+			// test to update  
+	}
+
+}
